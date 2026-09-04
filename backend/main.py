29794f79835308a787df8b8fff665a81309a9764
Samiya Kashif <samiyakashif335@gmail.com>
2026-09-04 23:29:59 +0500

    Add user authentication: register, login, password hashing, JWT tokens

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,9 @@
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+from database import SessionLocal
+from models import User
+from schemas import UserCreate, UserLogin, Token
+from auth import hash_password, verify_password, create_access_token
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -6,6 +12,12 @@ import numpy as np
 import json
 
 app = FastAPI()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,6 +26,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 model = joblib.load('../ml/models/diabetes_model.pkl')
 scaler = joblib.load('../ml/models/scaler.pkl')
@@ -102,3 +115,31 @@ def predict(patient: PatientData):
         "risk_label": "Diabetic" if prediction == 1 else "Not Diabetic",
         "risk_probability": round(float(probability), 4)
     }
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        email=user.email,
+        hashed_password=hash_password(user.password),
+        full_name=user.full_name
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully", "user_id": new_user.id}
+
+
+@app.post("/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    access_token = create_access_token(data={"sub": str(db_user.id)})
+    return {"access_token": access_token, "token_type": "bearer"}
